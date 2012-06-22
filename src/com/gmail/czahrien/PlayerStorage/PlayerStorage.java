cf7bac0f0cd8edfc601a7e62b6eb8b2328b094b4
Ben <bdm8233@rit.edu>
2012-06-22 14:33:38 -0400

    Documentation

--- src/com/gmail/czahrien/PlayerStorage/PlayerStorage.java
+++ src/com/gmail/czahrien/PlayerStorage/PlayerStorage.java
@@ -8,21 +8,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerStorage extends JavaPlugin implements Runnable {
     
+    /**
+     * onEnable schedules a task which saves the PlayerStorage data.
+     */
     @Override
     public void onEnable() {
         getServer().getScheduler().scheduleSyncRepeatingTask(this, this, 900000, 900000);
     }
     
+    /**
+     * When the plugin is unloaded it saves all PlayerStorage data.
+     */
     @Override
     public void onDisable() {
         saveInstances();
     }
     
+    /**
+     * The repeating task saves all PlayerStorage instances.
+     */
     @Override
     public void run() {
         saveInstances();
     }
     
+    /**
+     * Save all instances of PlayerStorage.
+     */
     public void saveInstances() {
         getLogger().info("Saving PlayerStorage data...");
         PlayerStorageInstance.saveAll();
--- src/com/gmail/czahrien/PlayerStorage/PlayerStorageInstance.java
+++ src/com/gmail/czahrien/PlayerStorage/PlayerStorageInstance.java
@@ -1,6 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * File: PlayerStorageInstance.java
+ * Description: Contains a class which represents an instance of the
+ * PlayerStorage plugin's information storage about Players.
  */
 package com.gmail.czahrien.PlayerStorage;
 
@@ -13,20 +14,51 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 /**
- *
+ * A class representing a PlayerStorage instance storing the type it is templated
+ * over in dat files for each player.
  * @author Czahrien
  */
 public class PlayerStorageInstance<E extends Serializable> {
+    /**
+     * A HashMap containing all (name,instance) pairs that exist.
+     */
+    private static HashMap<String,PlayerStorageInstance<?>> instances = new HashMap<>();  
+    /** 
+     * The path to the directory containing sub-directories for each instance.
+     */
     private static final String PATH = "plugins/PlayerStorage/";
+    /**
+     * The logger to which error messages are written.
+     */
+    private static final Logger log = Bukkit.getServer().getPluginManager().getPlugin("PlayerStorage").getLogger();
+    
+    /**
+     * The complete path to this instance's sub-directory.
+     */
     private String myPath;
-    private static HashMap<String,PlayerStorageInstance<?>> instances = new HashMap<>(); 
+
+    /**
+     * A HashMap contianing all (name,E) pairs that are a part of this instance.
+     */
     private HashMap<String,E> myMap;
+    /**
+     * A HashSet containing all entries that need to be saved.
+     */
     private HashSet<String> myDirtyEntries;
+    /**
+     * The name of this PlayerStorageInstance
+     */
     private String myName;
     
+    /**
+     * Creates a new PlayerStorageInstance with the specified name.
+     * @param name The name of this instance.
+     */
     private PlayerStorageInstance(String name) {
         myName = name;
         myPath = PATH + myName + "/";
@@ -34,7 +66,9 @@ public class PlayerStorageInstance<E extends Serializable> {
         myMap = new HashMap<>();
         File dir = new File(myPath);
         if(dir.isDirectory()) {
+            // Iterate through all files in the directory.
             for(File f : dir.listFiles()) {
+                // We only want files with names in the format "name.dat"
                 String[] fileName = f.getName().split("\\.");
                 if(fileName.length == 2 && fileName[1].equals("dat")) {
                     try {
@@ -48,10 +82,12 @@ public class PlayerStorageInstance<E extends Serializable> {
                 }
             }
         } else if(dir.isFile()) {
-            // TODO: ERROR: Expected directory, got file.
-            System.err.println("ERROR: Expected directory, got file.");
+            log.info("ERROR: Expected directory for " + name + " but got a non-directory file.");
         } else {
-            dir.mkdirs();
+            log.info("Creating directory structure for " + myPath + "...");
+            if( !dir.mkdirs() ) {
+                log.info("ERROR: Could not create the directory structure!");
+            } 
         }
     }
     
@@ -73,18 +109,29 @@ public class PlayerStorageInstance<E extends Serializable> {
     public void save() {
         for(String name : myDirtyEntries ) {
             try {
-                // We write to a temporary file in case if the server is
-                // forcefully terminated. In this case no player's data will
-                // be corrupt, only old.
-                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(myPath + name + ".dat.tmp"));
+                // We will write to a temporary file in case if the server is
+                // forcefully terminated or crashes during saving. 
+                // In this case no player's data will be corrupt if something
+                // goes wrong elsewhere but it might be old.
+                File f = new File(myPath + name + ".dat.tmp");
+                
+                // TODO: Check if f's name is a legal file name!
+                // TODO: It is conceivable that a malicious user on an offline
+                // server could set their name to the relative path a file that
+                // could cause damage elsewhere. 
+                
+                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
                 oos.writeObject(myMap.get(name));
                 oos.close();
+                
+                // Delete the old file.
                 File dat = new File(myPath + name + ".dat");
                 dat.delete();
+                // Rename the new file.
                 File tmp = new File(myPath + name + ".dat.tmp");
                 tmp.renameTo(dat);
             } catch(IOException e) {
-                System.err.println("ERROR: Could not save " + myName + " for " + name + ". Did you delete a directory?");
+                log.info("ERROR: Could not save " + myName + " for " + name + ". Did you delete a directory?");
             }
         }
         myDirtyEntries.clear();
